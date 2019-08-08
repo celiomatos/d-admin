e8cba4779773d660e0ce89d66ae8b9d3e2a04e77
Celio Roberto Martins de Matos <celio.matos@ITBAM.ORG.BR>
2019-08-08 17:03:31 -0400

    ad5a4d55D

--- src/main/java/br/com/dadmin/DAdminApplication.java
+++ src/main/java/br/com/dadmin/DAdminApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableAdminServer
 public class DAdminApplication extends SpringBootServletInitializer {
